902d9e4333d298c26918ffba9b3f0cac2aa3fa76
emCog <robin.leonard@gmail.com>
2020-12-02 23:25:25 +0000

    all routes working

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,8 +5,21 @@ from src.calculator import *
 def index():
     return 'Hello calculator'
 
-@app.route('/calc/<num_1>/<num_2>')
+@app.route('/add/<num_1>/<num_2>')
 def add_url(num_1, num_2):
     return add(num_1, num_2)
 
+@app.route('/subtract/<num_1>/<num_2>')
+def subtract_url(num_1, num_2):
+    return subtract(num_1, num_2)
+
+
+@app.route('/multiply/<num_1>/<num_2>')
+def multiply_url(num_1, num_2):
+    return multiply(num_1, num_2)
+
+
+@app.route('/divide/<num_1>/<num_2>')
+def divide_url(num_1, num_2):
+    return divide(num_1, num_2)
 
