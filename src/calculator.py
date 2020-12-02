9a477462721f6f59a877f42869244b5a517cda71
emCog <robin.leonard@gmail.com>
2020-12-02 22:56:40 +0000

    test refactored to meet schema & passed

--- src/calculator.py
+++ src/calculator.py
@@ -17,5 +17,8 @@ def multiply(num_1, num_2):
     ans = val_1 * val_2
     return str(ans)
 
-# def divide(num_1, num_2):
-#     pass
+def divide(num_1, num_2):
+    val_1 = int(num_1)
+    val_2 = int(num_2)
+    ans = val_1 // val_2
+    return str(ans)
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -13,8 +13,8 @@ class TestCalulator(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual('0', subtract(2, 2))
 
-    # def test_divide(self):
-    #     self.assertEqual(1, divide(2, 2))
+    def test_divide(self):
+        self.assertEqual('1', divide(2, 2))
 
     def test_multiply(self):
         self.assertEqual('8', multiply(2,4))
